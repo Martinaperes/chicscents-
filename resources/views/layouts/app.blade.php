ab767146a419acebc88213c53d984fd49d1671a8
Martina <1peresmartina@gmail.com>
2026-02-23 16:51:50 +0300

    Auto-update: app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/jpeg" href="{{ asset('images/hero/chicscentslogo.jpeg') }}">
     <title>@yield('title', 'Chic Scents | Luxury Fragrances')</title>
     
     <!-- Tailwind CSS -->
@@ -185,7 +186,7 @@
     </div>
 
     <!-- Main Content -->
-    <main>
+    <main class="relative pt-20">
         @yield('content')
     </main>
 
