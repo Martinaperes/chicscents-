243bf43b88f153ba7c5f8944b86333c3fb7047da
Martina <1peresmartina@gmail.com>
2026-02-25 19:20:36 +0300

    Auto-update: laravel-chatbot.php

--- config/laravel-chatbot.php
+++ config/laravel-chatbot.php
@@ -3,6 +3,6 @@
 return [
     'enabled' => true,
     'bot_name' => 'ChatBuddy',
-    'welcome_message' => 'Hello welcome to chic scents! I am your Chic scent Chatbot 🤖',
+    'welcome_message' => 'Hello welcome to ScentCepts! I am your Chic scent Chatbot 🤖',
     'default_response' => 'I didn\'t understand that. Can you please rephrase?',
 ];
