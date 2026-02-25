6d69cb1ff68aead57f77ae1b0648a7ee706da861
Martina <1peresmartina@gmail.com>
2026-02-25 19:22:27 +0300

    Auto-update: index.blade.php

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Shop All Fragrances | Chic Scents')
+@section('title', 'Shop All Fragrances | ScentCepts')
 
 @section('content')
     <!-- Top Spacer -->
